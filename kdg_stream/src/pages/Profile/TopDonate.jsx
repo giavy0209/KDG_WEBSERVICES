9e823b1b5926db2ebea2fe86bff5a86f9a8c636f
giavy0209 <49144082+giavy0209@users.noreply.github.com>
2021-04-26 12:06:13 +0700

    d

--- kdg_stream/src/pages/Profile/TopDonate.jsx
+++ kdg_stream/src/pages/Profile/TopDonate.jsx
@@ -1,10 +1,31 @@
+import { useMemo, useState } from 'react';
+import callAPI from '../../axios';
 import { AssetBox, Table } from '../../components';
+import { useLanguageLayerValue } from '../../context/LanguageLayer';
 
 export default function TopDonate () {
+    const [{ language, profile }] = useLanguageLayerValue();
+    const [TopDonate , setTopDonate] = useState([])
+
+    useMemo(() => {
+        callAPI.get('/top_donate').then(res => {
+            setTopDonate([...res.data])
+        })
+    },[])
+
+    const storageHead = useMemo(() => {
+        return [
+          {
+            key: 'from',
+            name: profile[language].gift,
+            render: from => from.email
+          },
+        ];
+      }, [language, profile]);
     return (
         <>
             <AssetBox collapse={false} title=''>
-                
+                <Table dataHead={storageHead} dataBody={TopDonate}/>
             </AssetBox>
         </>
     )
