d285a9f2ec3b1950c8513d61e56a980fc88a4d60
giavy0209 <49144082+giavy0209@users.noreply.github.com>
2021-06-08 11:22:35 +0700

    a

--- kinglive_v2/src/App.jsx
+++ kinglive_v2/src/App.jsx
@@ -4,6 +4,7 @@ import './assets/scss/styles.scss'
 import Header from './components/Header'
 import Sidebar from './components/Sidebar'
 import Home from './pages/Home'
+import Live from './pages/Live'
 import Upload from './pages/Upload'
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
       <main className={`${IsOpenSidebar ? 'small' : ''}`}>
         <Switch>
           <Route path='/' component={Home} exact />
+          <Route path='/live' component={Live} exact />
           <Route path='/upload' component={Upload} exact />
         </Switch>
       </main>
