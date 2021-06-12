0c6a83aa11adcda311860a2a8633b2f70537f89e
Vu Vu <vuvu2259@gmail.com>
2021-06-12 15:03:55 +0700

    x

--- kinglive_v2/src/pages/WatchLive/WatchLive.jsx
+++ kinglive_v2/src/pages/WatchLive/WatchLive.jsx
@@ -1,8 +1,10 @@
-import '../../assets/scss/watchlive.scss'
-import ReactHlsPlayer from 'react-hls-player'
-import { PLAY_STREAM } from '../../constant'
 import { useEffect, useMemo, useState } from 'react'
+import ReactHlsPlayer from 'react-hls-player'
+import '../../assets/scss/watchlive.scss'
+import shareSVG from '../../assets/svg/share.svg'
+import avatest from '../../assets/svg/avatest.png'
 import callAPI from '../../axios'
+import { PLAY_STREAM } from '../../constant'
 
 export default function WatchLive() {
   const [streamData, setStreamData] = useState({})
@@ -41,8 +43,41 @@ export default function WatchLive() {
         <div className='watchlive__titleVideo'>
           Gầy On Stream Hướng Dẫn Ae Cách Nạp Và Rút KDG - CMT NHỮNG THẮC MẮC CỦA BẠN
         </div>
+
+        <div className='watchlive__viewVideo'>
+          <span>11 views</span>
+          <span> • </span>
+          <span>11 minutes ago</span>
+          <span> | </span>
+          <span className='sharebutton'>
+            <img src={shareSVG} alt='' />
+            Share
+          </span>
+        </div>
+
+        <div className='watchlive__infoVideo'>
+          <div>
+            <img src={avatest} alt='' />
+          </div>
+          <div>
+            <div>An thuong gia</div>
+            <div>176 followers</div>
+            <div>
+              Cảm ơn mọi người ghé thăm livestream của em và đừng quên ủng hộ em 1 follow và 1 lời
+              chào ở phần cmt nha. Phần quà của mọi người là động lực để em đồng hành cùng mọi người
+              trong thời gian tới nha. Chúc mọi người xem stream vui vẻ và có thật nhiều sức khỏe
+              hihi.
+            </div>
+          </div>
+        </div>
       </div>
-      <div className='watchlive__right'></div>
+
+      <div className='watchlive__right'>
+        <div className='watchlive__chatContainer'>
+          <div></div>
+          <div>Hide chat</div>
+        </div>
+      </div>
     </div>
   )
 }
